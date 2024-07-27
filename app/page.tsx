2b0eadd30cac9bb2d4badc04df96b4f9669facd7
Charles Alexander <chrlsxnd@amazon.com>
2024-07-27 16:25:45 -0700

    Removed unescaped apostrophe in Properties.tsx
     Removed unneeded import in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react'
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Properties from "@/components/Properties";
-import Problem from "@/components/Problem";
+// import Problem from "@/components/Problem";
 import FeaturesAccordion from "@/components/FeaturesAccordion";
 import Pricing from "@/components/Pricing";
 import FAQ from "@/components/FAQ";

44df141c0a302871de09e0d6ac14ebb1a782c935
Charles Alexander <chrlsxnd@amazon.com>
2024-07-28 00:21:49 -0700

    Basic prototype of embedded widget in property 1 page

--- components/Property1.tsx
+++ components/Property1.tsx
@@ -21,6 +21,16 @@ const Property1 = () => {
         </div>
       </div>
 
+      {/* Container with minimum width and height */}
+      <div className="min-w-[320px] h-[900px] bg-gray-200 p-4 mx-auto">
+      <iframe 
+        id="booking-iframe" 
+        sandbox="allow-top-navigation allow-scripts allow-same-origin" 
+        style={{ width: '100%', height: '900px' }}
+        frameBorder="0" 
+        src="https://booking.hospitable.com/widget/9ca01362-9da8-44f3-9e64-18080aceba27/493270"></iframe>
+      </div>
+
 
 
       

943ae6d21aecc7586427a4c6a442e2a37712d0d8
Charles Alexander <chrlsxnd@amazon.com>
2024-07-28 00:33:29 -0700

    Removed unnecessary pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import { Suspense } from 'react'
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Properties from "@/components/Properties";
-import FeaturesAccordion from "@/components/FeaturesAccordion";
-import Pricing from "@/components/Pricing";
 import FAQ from "@/components/FAQ";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
@@ -23,8 +21,6 @@ export default function Home() {
         <Property1 />
         <Property2 />
         <Property3 />
-        <FeaturesAccordion />
-        <Pricing />
         <FAQ />
         <CTA />
       </main>

be0d3be0e214acc7d10c5f1f5d41f8e67813162e
Charles Alexander <chrlsxnd@amazon.com>
2024-08-22 22:13:59 -0700

    Added tailwind prose support and implemented in the About the Property section

--- components/Property1.tsx
+++ components/Property1.tsx
@@ -60,26 +60,31 @@ const Property1 = () => {
             </div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="flex justify-center p-10">
-                <p>
-                The guest suite is located on the basement floor of a detached home, 
-                and is completely seperate from the unit above. The entrance opens to a 
-                stylish, newly renovated kitchen, a living space with West Elm furnishings 
-                and a smart TV, and a dining set for four. The kitchen is fully stocked with 
-                everything you need to cook during your stay, as well as coffee and tea 
-                supplies. The suite has two cozy bedrooms - the first features a king bed 
-                and the second has two twin beds. The bathroom is also newly renovated and 
-                features a tile shower surround and full sized bathtub. A laundry room and 
-                an office with a workstation round out the unit, and fiber optic internet 
-                ensures a reliable and high performance connection for guests looking to 
-                stream or work remotely during their stay.{"\n"}
+        <div className="grid grid-cols-1 mx-auto md:grid-cols-2 gap-4 ">
+            <div className="flex justify-center p-4 lg:p-10">
+                <article className="prose lg:prose-l">
+                    <h3> About the property</h3>
+                    <p>
+                    The guest suite is located on the basement floor of a detached home, 
+                    and is completely seperate from the unit above. The entrance opens to a 
+                    stylish, newly renovated kitchen, a living space with West Elm furnishings 
+                    and a smart TV, and a dining set for four. The kitchen is fully stocked with 
+                    everything you need to cook during your stay, as well as coffee and tea 
+                    supplies. The suite has two cozy bedrooms - the first features a king bed 
+                    and the second has two twin beds. The bathroom is also newly renovated and 
+                    features a tile shower surround and full sized bathtub. A laundry room and 
+                    an office with a workstation round out the unit, and fiber optic internet 
+                    ensures a reliable and high performance connection for guests looking to 
+                    stream or work remotely during their stay.
+                    </p>
 
-                Although this unit it freshly renovated, the home itself is older and sound 
-                travels fairly well. This makes this unit ideal for groups who are happy 
-                to relax quietly once quiet hours begin. The tenants upstairs are young 
-                professionals and are generally in bed early.
-                </p>
+                    <p>
+                    Although this unit it freshly renovated, the home itself is older and sound 
+                    travels fairly well. This makes this unit ideal for groups who are happy 
+                    to relax quietly once quiet hours begin. The tenants upstairs are young 
+                    professionals and are generally in bed early.
+                    </p>
+                </article>
 
             </div>
 

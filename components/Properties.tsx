eec10e9ff442b01ef6e07ba5bdbf0e38288bbd94
Charles Alexander <chrlsxnd@amazon.com>
2024-08-25 11:59:52 -0700

    Aesthetic changes to Properties.tsx, two columns of cards instead of 3 and a narrower max-w-screen-xl instead of 2xl.

--- components/Properties.tsx
+++ components/Properties.tsx
@@ -9,24 +9,23 @@ import PropertyCard from "./PropertyCard";
 
 const Properties = () => {
   return (
-    <section className="bg-base-100 overflow-hidden max-w-screen-2xl" id="properties" data-theme="mybrand" style={{ margin: '0 auto' }}>
-      <div className="py-8 px-8 max-w-5xl mx-auto">
-        <div className="flex flex-col text-center w-full mb-10">
-          <p className="font-bold text-3xl lg:text-5xl tracking-tight text-primary mb-8">Our Properties</p>
-          <h2 className="font-medium text-xl lg:text-2xl tracking-tight">
-            {'We\'ve got you covered from Shoreline to Tukwila'}
-          </h2>
-        </div>
-      </div>
-
-      <div className="grid gap-4 grid-cols-3 grid-rows-2" id="testgrid">
+    <section className="bg-base-100 overflow-hidden max-w-screen-xl" id="properties" data-theme="mybrand" style={{ margin: '0 auto' }}>
+        <div className="py-8 px-8 max-w-5xl mx-auto">
+            <div className="flex flex-col text-center w-full mb-10">
+                <p className="font-bold text-3xl lg:text-5xl tracking-tight text-primary mb-8">Our Properties</p>
+                <h2 className="font-medium text-xl lg:text-2xl tracking-tight">
+                {'We\'ve got you covered from Shoreline to Tukwila'}
+                </h2>
+            </div>
         </div>
 
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        < PropertyCard href="/listings/sound-breeze"        title="Sound Breeze"    imageSrc={image1}/>
-        < PropertyCard href="/listings/songbird-suite"      title="Songbird Suite"  imageSrc={image2}/>
-        < PropertyCard href="/listings/seattle-launchpad"   title="Launchpad"       imageSrc={image3}/>
-    </div>
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-10">
+            < PropertyCard href="/listings/sound-breeze"        title="Sound Breeze"    imageSrc={image1}/>
+            < PropertyCard href="/listings/songbird-suite"      title="Songbird Suite"  imageSrc={image2}/>
+            < PropertyCard href="/listings/seattle-launchpad"   title="Launchpad"       imageSrc={image3}/>
+        </div>
+
+        <div className="flex w-full h-10"/>
 
     </section>
   );

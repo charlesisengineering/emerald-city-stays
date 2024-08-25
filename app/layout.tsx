8a53275141f453050f84d093a28677c1ff195683
Charles Alexander <chrlsxnd@amazon.com>
2024-08-24 20:05:06 -0700

    Adding Speed Insights per https://vercel.com/charles-projects-2e0a1f78/emerald_city_stays/speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import ClientLayout from "@/components/LayoutClient";
 import config from "@/config";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const font = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         {/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
         <ClientLayout>{children}</ClientLayout>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
